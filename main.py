e200a46557840483178757a97c45f505b2c0a0c2
Santiago-rom <santyantonioromeromercado@gmail.com>
2026-04-14 10:27:58 -0400

    Correction main pour corriger le graphique

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from interface2 import afficher_resultat
 # lancer les calculs
 liste_x, liste_y, liste_z, verdict = simuler_tir(force_impact, position_x, position_y)
 
-# afficher les résultats
-afficher_resultat(liste_y, liste_z, verdict, force_impact)
+# afficher vue du dessus (X-Y)
+afficher_resultat(liste_x, liste_y, verdict, force_impact)
