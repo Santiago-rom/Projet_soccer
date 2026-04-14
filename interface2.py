4ad3b7c8f38c630a06a59d231ccc1777a4c9b4e9
Santiago-rom <santyantonioromeromercado@gmail.com>
2026-04-14 10:32:53 -0400

    Correction interface 2 graphique

--- interface2.py
+++ interface2.py
@@ -1,22 +1,23 @@
 
 import matplotlib.pyplot as plt
 
-#Creer la fonction qui va afficher le résultat sous forme de graphique avec des paramétres y et z
-def afficher_resultat(y,z , verdict , force  ):
+#Creer la fonction qui va afficher le résultat sous forme de graphique avec des paramétres x et y
+def afficher_resultat(x,y, verdict , force  ):
 
-#Trace le graphique dont le y est l'axe horizontal et le z l'axe vertical avec une ligne et des points a chaque position du ballon
-    plt.plot(y, z , color="black")
-    plt.scatter(y,z , color="green")
-
+#Trace le graphique dont le x est l'axe horizontal et le y l'axe vertical avec une ligne et des points a chaque position du ballon
+    plt.plot(x,y , color="black")
+    plt.scatter(x,y , color="green")
+    
 # On determine le nom des axes horizontales et verticales et aussi le titre du graphique
-    plt.xlabel("La distance vers le but en (m)")
-    plt.ylabel("La hauteur du ballon en (m)")
+    plt.ylabel("La distance vers le but en (m)")
+    plt.xlabel("La distance latéral du ballon en (m)")
     plt.title("La trajectoire du tir ")
 
-#Le ballon peut parcourir une distance entre 0 a 60 m sur l'axe horizontale . Car pour qu'un tir soit considérer comme un but il doit pouvoir franchir la ligne blanche qui est de 52,5 m
-    plt.xlim(0,60)
-#La ballon ne peut pas depasser 3 m de hauteur
-    plt.ylim(0,3)
+#Le ballon peut parcourir une distance entre 0 a 55 m sur l'axe verticale . Car pour qu'un tir soit considérer comme un but il doit pouvoir franchir la ligne blanche qui est de 52,5 m
+    plt.ylim(0,55)
+#Limite horizontale du terrain
+    plt.xlim(-35,35)
+    
 #Ajoute une grille au graphique
     plt.grid()
 #Ajout de la legende pour differencier la trajectoire du ballon avec sa position
